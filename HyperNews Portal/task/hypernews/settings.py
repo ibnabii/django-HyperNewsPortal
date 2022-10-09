52ea3dd140c0e5a8dbb78a89d1a356dfd55cbacc
ibnabii <someone@somewhere.net>
2022-10-09 21:46:04 +0200

    Stage 2
    (cherry picked from commit bd1dc135b4e7c9aaca6b11e96b9b8516dfe70c10)

--- HyperNews Portal/task/hypernews/settings.py
+++ HyperNews Portal/task/hypernews/settings.py
@@ -74,12 +74,14 @@ WSGI_APPLICATION = 'hypernews.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+NEWS_JSON_PATH = BASE_DIR / 'news.json'
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
--- HyperNews Portal/task/news/views.py
+++ HyperNews Portal/task/news/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpResponse, Http404
+from json import load
+
 
 def home(request):
     return render(request, 'news/index.html')
+
+
+def article(request, link):
+    with open(settings.NEWS_JSON_PATH, "r") as json_file:
+        articles = load(json_file)
+    article_to_display = None
+    for article in articles:
+        if article.get("link", None) == int(link):
+            article_to_display = article
+    if article_to_display is None:
+        raise Http404
+    context = {"news": article_to_display}
+    return render(request, 'news/article.html', context=context)
